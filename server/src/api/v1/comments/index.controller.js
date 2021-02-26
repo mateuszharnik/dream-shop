e63ccda7a5d2bf82be7c0325d7c9a00c2eb376b0
Mateusz Harnik <36229431+mateuszharnik@users.noreply.github.com>
2021-02-26 14:11:09 +0100

    :sparkles: Order summary on the admin panel
    Create OrderSummary component. Update comment model on the server.
    Add pagination to the comments on the server. Change user information
    in the comment on the server.

--- server/src/api/v1/comments/index.controller.js
+++ server/src/api/v1/comments/index.controller.js
@@ -1,24 +1,36 @@
 const { commentSchema } = require('./index.model');
 const { dbIdSchema } = require('../../../models');
-const { commentsDB, productsDB } = require('../../../db');
+const { commentsDB, productsDB, usersDB } = require('../../../db');
 const { responseWithError } = require('../../../helpers/errors');
 const { purify } = require('../../../helpers/sanitize');
 
 const getComments = async (req, res, next) => {
-  const { product_id = '' } = req.query;
+  const { product_id = '', sort = 'desc' } = req.query;
+  let { skip = 0, limit = 6 } = req.query;
+
+  skip = parseInt(skip, 10) || 0;
+  limit = parseInt(limit, 10) || 6;
+
+  skip = skip < 0 ? 0 : skip;
+
+  limit = Math.min(50, Math.max(1, limit));
 
   try {
     const query = { deleted_at: null };
+    const options = {
+      skip: Number(skip),
+      limit: Number(limit),
+      sort: {
+        created_at: sort === 'desc' ? -1 : 1,
+      },
+    };
 
     if (product_id) {
       query.product_id = product_id;
     }
 
-    const comments = await commentsDB.find(query, {
-      sort: {
-        created_at: -1,
-      },
-    });
+    const total = await commentsDB.count(query);
+    const comments = await commentsDB.find(query, options);
 
     if (!comments) {
       return responseWithError(
@@ -29,7 +41,42 @@ const getComments = async (req, res, next) => {
       );
     }
 
-    res.status(200).json(comments);
+    const arrayOfIDs = comments
+      .filter((comment) => comment.user_id)
+      .map((comment) => comment.user_id);
+
+    const users = await usersDB.find({ _id: { $in: arrayOfIDs } });
+
+    const commentsWithUserInfo = comments.map((comment) => {
+      const userInfo = {};
+
+      if (!comment.user_id) {
+        userInfo.author = 'Anonim';
+        userInfo.author_image = '';
+      } else {
+        const { username, avatar } = users.find(
+          (user) => user._id.toString() === comment.user_id,
+        );
+
+        userInfo.author = username;
+        userInfo.author_image = avatar;
+      }
+
+      return {
+        ...comment,
+        ...userInfo,
+      };
+    });
+
+    res.status(200).json({
+      total,
+      pagination: {
+        skip,
+        limit,
+        remaining: total - (skip + limit) > 0,
+      },
+      comments: commentsWithUserInfo,
+    });
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
@@ -61,7 +108,24 @@ const getComment = async (req, res, next) => {
       return responseWithError(res, next, 404, 'Komentarz nie istnieje.');
     }
 
-    res.status(200).json(comment);
+    const authorInfo = {
+      author: 'Anonim',
+      author_image: '',
+    };
+
+    if (req.user && req.user._id) {
+      const user = await usersDB.findOne({ _id: req.user._id });
+
+      if (user) {
+        authorInfo.author = user.username;
+        authorInfo.author_image = user.avatar;
+      }
+    }
+
+    res.status(200).json({
+      ...comment,
+      ...authorInfo,
+    });
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
@@ -89,7 +153,12 @@ const deleteComments = async (req, res, next) => {
     );
 
     if (!deletedComments) {
-      return responseWithError(res, next, 500, 'Nie udało się usunąć komentarza.');
+      return responseWithError(
+        res,
+        next,
+        500,
+        'Nie udało się usunąć komentarza.',
+      );
     }
 
     res.status(200).json({
@@ -124,16 +193,45 @@ const deleteComment = async (req, res, next) => {
       return responseWithError(res, next, 500, 'Komentarz nie istnieje.');
     }
 
+    if (
+      req.user._id !== comment.user_id
+      || req.user.roles.indexOf('administrator') === -1
+    ) {
+      return responseWithError(res, next, 500, 'Brak dostępu.');
+    }
+
+    const authorInfo = {
+      author: 'Anonim',
+      author_image: '',
+    };
+
+    if (req.user && req.user._id) {
+      const user = await usersDB.findOne({ _id: req.user._id });
+
+      if (user) {
+        authorInfo.author = user.username;
+        authorInfo.author_image = user.avatar;
+      }
+    }
+
     const deletedComment = await commentsDB.findOneAndUpdate(
       { _id: params.id },
       { $set: { deleted_at: new Date() } },
     );
 
     if (!deletedComment) {
-      return responseWithError(res, next, 500, 'Nie udało się usunąć komentarza.');
+      return responseWithError(
+        res,
+        next,
+        500,
+        'Nie udało się usunąć komentarza.',
+      );
     }
 
-    res.status(200).json(deletedComment);
+    res.status(200).json({
+      ...deletedComment,
+      ...authorInfo,
+    });
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
@@ -155,16 +253,39 @@ const updateComment = async (req, res, next) => {
     );
   }
 
-  const { schemaError, data } = commentSchema(req.body, false, false);
-
-  if (schemaError) {
-    return responseWithError(res, next, 400, schemaError.details[0].message);
-  }
-
-  data.purify_content = purify(data.content);
-
   try {
-    const product = await productsDB.findOne({ _id: data.product_id, deleted_at: null });
+    const comment = await commentsDB.findOne({ _id: params.id });
+
+    if (!comment || (comment && comment.deleted_at)) {
+      return responseWithError(res, next, 500, 'Komentarz nie istnieje.');
+    }
+
+    if (req.user._id !== comment.user_id) {
+      return responseWithError(
+        res,
+        next,
+        500,
+        'Nie można edytować anonimowych komentarzy.',
+      );
+    }
+
+    const body = {
+      ...req.body,
+      user_id: comment.user_id,
+    };
+
+    const { schemaError, data } = commentSchema(body, false, false);
+
+    if (schemaError) {
+      return responseWithError(res, next, 400, schemaError.details[0].message);
+    }
+
+    data.purify_content = purify(data.content);
+
+    const product = await productsDB.findOne({
+      _id: data.product_id,
+      deleted_at: null,
+    });
 
     if (!product || (product && product.deleted_at)) {
       return responseWithError(
@@ -175,10 +296,30 @@ const updateComment = async (req, res, next) => {
       );
     }
 
-    const comment = await commentsDB.findOne({ _id: params.id });
+    const authorInfo = {
+      author: 'Anonim',
+      author_image: '',
+    };
 
-    if (!comment || (comment && comment.deleted_at)) {
-      return responseWithError(res, next, 500, 'Komentarz nie istnieje.');
+    if (req.user && req.user._id) {
+      const user = await usersDB.findOne({
+        _id: req.user._id,
+        deleted_at: null,
+      });
+
+      if (!user || (user && user.deleted_at)) {
+        return responseWithError(
+          res,
+          next,
+          404,
+          'Nie możesz zaktualizować komentarza ponieważ użytkownik o podanym numerze ID nie istnieje.',
+        );
+      }
+
+      if (user) {
+        authorInfo.author = user.username;
+        authorInfo.author_image = user.avatar;
+      }
     }
 
     const updatedComment = await commentsDB.findOneAndUpdate(
@@ -200,7 +341,10 @@ const updateComment = async (req, res, next) => {
       );
     }
 
-    res.status(200).json(updatedComment);
+    res.status(200).json({
+      ...updatedComment,
+      ...authorInfo,
+    });
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
@@ -209,7 +353,12 @@ const updateComment = async (req, res, next) => {
 };
 
 const addComment = async (req, res, next) => {
-  const { schemaError, data } = commentSchema(req.body, false, false);
+  const body = {
+    ...req.body,
+    user_id: req.user && req.user._id ? req.user._id : '',
+  };
+
+  const { schemaError, data } = commentSchema(body, false, false);
 
   if (schemaError) {
     return responseWithError(res, next, 400, schemaError.details[0].message);
@@ -218,7 +367,10 @@ const addComment = async (req, res, next) => {
   data.purify_content = purify(data.content);
 
   try {
-    const product = await productsDB.findOne({ _id: data.product_id, deleted_at: null });
+    const product = await productsDB.findOne({
+      _id: data.product_id,
+      deleted_at: null,
+    });
 
     if (!product || (product && product.deleted_at)) {
       return responseWithError(
@@ -227,6 +379,32 @@ const addComment = async (req, res, next) => {
         404,
         'Nie możesz dodać komentarza do produktu który nie istnieje.',
       );
+    }
+
+    const authorInfo = {
+      author: 'Anonim',
+      author_image: '',
+    };
+
+    if (req.user && req.user._id) {
+      const user = await usersDB.findOne({
+        _id: req.user._id,
+        deleted_at: null,
+      });
+
+      if (!user || (user && user.deleted_at)) {
+        return responseWithError(
+          res,
+          next,
+          404,
+          'Nie możesz dodać komentarza ponieważ użytkownik o podanym numerze ID nie istnieje.',
+        );
+      }
+
+      if (user) {
+        authorInfo.author = user.username;
+        authorInfo.author_image = user.avatar;
+      }
     }
 
     const comment = await commentsDB.insert({
@@ -245,7 +423,10 @@ const addComment = async (req, res, next) => {
       );
     }
 
-    res.status(200).json(comment);
+    res.status(200).json({
+      ...comment,
+      ...authorInfo,
+    });
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
